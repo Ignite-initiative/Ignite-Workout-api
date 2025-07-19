5119c1d19afa1afabd8a6cee8a750ee4267a1b41
Leonardo <leonardolccabral@gmail.com>
2025-07-19 01:26:14 -0300

    test/refactor: improve switch tests using a describe and improve message

--- test/integration/api/register/post.test.ts
+++ test/integration/api/register/post.test.ts
@@ -1,32 +1,37 @@
 import prisma from '../../../../src/utils/prismaClient';
 
-beforeAll(cleanUsers);
+describe("Get on /register", () => {
 
-async function cleanUsers() {
-  {
-    await prisma.$queryRaw`DELETE FROM "User"`
-  }
-}
+  beforeEach(cleanUsers);
 
-test("GET on /register", async () => {
-  const userRegister = {
-    name: "Leonado",
-    email: "leo@email.com",
-    password: "123456",
-    telephone: "6199982756",
-    height: 183,
-    weight: 99
+  async function cleanUsers() {
+    {
+      await prisma.$queryRaw`DELETE FROM "User"`
+    }
   }
 
-  const result = await fetch('http://localhost:3000/register', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(userRegister)
+  test("Should return a status 200 on success", async () => {
+    const userRegister = {
+      name: "Leonado",
+      email: "leo@email.com",
+      password: "123456",
+      telephone: "6199982756",
+      height: 183,
+      weight: 99
+    }
+
+    const result = await fetch('http://localhost:3000/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userRegister)
+    })
+
+    const convertResult = await result.json()
+    expect(result.status).toBe(201)
+    expect(convertResult.message).toBe('user created')
   })
-
-  const convertResult = await result.json()
-  expect(result.status).toBe(201)
-  expect(convertResult.message).toBe('user created')
 })
+
+
