5eb2ef3610ad5ba71527ae5ec3cd271e4a5e233f
Pedro Henrique <76178088+pedrohenrique11@users.noreply.github.com>
2025-07-24 17:51:45 +0000

    refactor: remove redundant functions

--- src/repositories/prisma/prisma-user-repository.ts
+++ src/repositories/prisma/prisma-user-repository.ts
@@ -3,12 +3,6 @@ import { Prisma } from '@prisma/client'
 import { UserRepository } from '../user-repository'
 
 export class PrismaUserRository implements UserRepository {
-    async getAll() {
-        return await prisma.user.findMany()
-    }
-    async deleteAll() {
-        return await prisma.user.deleteMany({})
-    }
     async getByEmail(email: string) {
         return await prisma.user.findUnique({
             where: {
@@ -18,10 +12,9 @@ export class PrismaUserRository implements UserRepository {
     }
 
     async create(data: Prisma.UserCreateInput) {
-        const user = await prisma.user.create({
+        return await prisma.user.create({
             data,
         })
 
-        return user
     }
 }
