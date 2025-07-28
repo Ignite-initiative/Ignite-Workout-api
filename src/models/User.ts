eab63dad25207faefe39d744d399447eb76c7dd1
Pedro Henrique <76178088+pedrohenrique11@users.noreply.github.com>
2025-07-28 15:56:18 +0000

    refactor: combine user repository and service into MVC model

---
+++ src/models/User.ts
@@ -0,0 +1,54 @@
+import prisma from '../utils/prismaClient'
+import { Prisma, User } from '@prisma/client'
+import { EmailAlreadyExists } from "../errors/email-already-exists";
+import { hash } from "bcryptjs";
+
+
+interface RegisterUserData {
+    name: string
+    email: string
+    password: string
+    telephone: string | null
+    height: number
+    weight: number
+}
+
+export class UserModel {
+    static async findByEmail(email: string): Promise<User | null> {
+        return prisma.user.findUnique({ where: { email } })
+    }
+
+    static async findById(id: string): Promise<User | null> {
+        return prisma.user.findUnique({ where: { id } })
+    }
+
+    static async create(data: Prisma.UserCreateInput): Promise<User> {
+        return prisma.user.create({ data })
+    }
+
+    static async register({
+        name,
+        email,
+        password,
+        telephone,
+        height,
+        weight
+    }: RegisterUserData): Promise<User> {
+
+        const emailUsed = await this.findByEmail(email)
+        if (emailUsed) throw new EmailAlreadyExists()
+
+        const password_hash = await hash(password, 10)
+
+        const user = await this.create({
+            name,
+            email,
+            password_hash,
+            telephone,
+            height,
+            weight
+        })
+
+        return user
+    }
+}
