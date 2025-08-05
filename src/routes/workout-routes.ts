ba2558484f480baa3e8389e9361b4b0f5130a03c
Pedro Henrique <76178088+pedrohenrique11@users.noreply.github.com>
2025-08-05 13:18:06 +0000

    feat: add remove workout routes

--- src/routes/workout-routes.ts
+++ src/routes/workout-routes.ts
@@ -4,7 +4,8 @@ import * as workout from "../controllers/workout-controller";
 const workoutRoutes = Router()
 
 workoutRoutes.post("/workout/register", workout.register)
-workoutRoutes.patch("/workout/:id/update/status", workout.complete)
+workoutRoutes.patch("/workout/:id/update/status", workout.updateStatus)
 workoutRoutes.patch("/workout/:id/update/date", workout.updateDate)
+workoutRoutes.delete("/workout/:id/delete", workout.remove)
 
 export default workoutRoutes;
