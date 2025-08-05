e21607cb2099347a3b60e46ad69e4c39a23a5bbd
Pedro Henrique <76178088+pedrohenrique11@users.noreply.github.com>
2025-08-05 11:36:31 +0000

    feat: add update-status route

--- src/routes/workout-routes.ts
+++ src/routes/workout-routes.ts
@@ -4,5 +4,6 @@ import * as workout from "../controllers/workout-controller";
 const workoutRoutes = Router()
 
 workoutRoutes.post("/workout/register", workout.register)
+workoutRoutes.patch("/workout/complete", workout.complete)
 
 export default workoutRoutes;
