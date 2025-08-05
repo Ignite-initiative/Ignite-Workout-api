9ff5b7dbc5bcbde4784db60d510d9beb120debfd
Pedro Henrique <76178088+pedrohenrique11@users.noreply.github.com>
2025-08-05 13:08:54 +0000

    feat: add workot update date router

--- src/routes/workout-routes.ts
+++ src/routes/workout-routes.ts
@@ -4,6 +4,7 @@ import * as workout from "../controllers/workout-controller";
 const workoutRoutes = Router()
 
 workoutRoutes.post("/workout/register", workout.register)
-workoutRoutes.patch("/workout/:id/status", workout.complete)
+workoutRoutes.patch("/workout/:id/update/status", workout.complete)
+workoutRoutes.patch("/workout/:id/update/date", workout.updateDate)
 
 export default workoutRoutes;
