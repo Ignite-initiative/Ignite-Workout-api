9ed2f04567f85e86bd65fb3a2156d2bcb976e4ac
Pedro Henrique <76178088+pedrohenrique11@users.noreply.github.com>
2025-08-06 18:25:28 +0000

    refactor: all model class

--- src/models/Workout.ts
+++ src/models/Workout.ts
@@ -1,5 +1,7 @@
+import { connect } from "http2";
 import prisma from "../utils/prismaClient";
-import { Workout } from "@prisma/client";
+import { Workout, Prisma } from "@prisma/client";
+import { workoutNotFound } from "../errors/workout/workout-errors";
 
 interface RegisterWorkoutData{
     name: string,
@@ -9,45 +11,47 @@ interface RegisterWorkoutData{
 }
 
 export class WorkoutModel{
-    static async findUnique(id: string){
+    static async create(data: Prisma.WorkoutCreateInput): Promise<Workout> {
+        return await prisma.workout.create({ data })
+    }
+    static async findUnique(id: string,  ): Promise<Workout | null>{
         return prisma.workout.findUnique( { where: { id } } ) 
     }
-
-    async register({ 
+    static async count(where?: Prisma.WorkoutWhereInput): Promise<number>{
+        return await prisma.workout.count({ where })
+    }
+    
+    async register({
         name, 
         date, 
         isCompleted, 
         userId 
     }: RegisterWorkoutData): Promise<Workout>{
-        return await prisma.workout.create({
-            data: { 
-                name,
-                date,
-                isCompleted,
-                user: {
-                    connect: { id: userId }
-                }
-            }
+        return await WorkoutModel.create({
+            name,
+            date,
+            isCompleted,
+            user: { connect: {id: userId}}
         })
-    }
-
+    }    
     async updateStatus(id: string) {
         const workout = await WorkoutModel.findUnique(id)
-
-        await prisma.workout.update({
-            where: {id: id},
-            data: {isCompleted: !workout?.isCompleted}
-        })
+        if(!workout) throw new workoutNotFound()
+            
+            await prisma.workout.update({
+                where: {id: id},
+                data: {isCompleted: !workout?.isCompleted}
+            })
+        }
+        async updateDate(id: string, date: Date) {
+            await prisma.workout.update({
+                where: {id: id},
+                data: {date: new Date(date)}
+            })
+        }
+        async delete(id: string) {
+            await prisma.workout.delete({
+                where: {id: id},
+            })
+        }
     }
-    async updateDate(id: string, date: Date) {
-        await prisma.workout.update({
-            where: {id: id},
-            data: {date: new Date(date)}
-        })
-    }
-    async delete(id: string) {
-        await prisma.workout.delete({
-            where: {id: id},
-        })
-    }
-}
