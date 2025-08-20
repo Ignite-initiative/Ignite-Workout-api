f7e42ba1160727c5e23d2c6622c23021fe9b7f4c
Pedro Henrique <76178088+pedrohenrique11@users.noreply.github.com>
2025-08-20 01:46:45 +0000

    reafactor: delete workout endpoint

--- src/routes/workout-routes.ts
+++ src/routes/workout-routes.ts
@@ -4,6 +4,6 @@ import * as workout from "../controllers/workout-controller";
 const workoutRoutes = Router()
 
 workoutRoutes.post("/workout/register", workout.register)
-workoutRoutes.delete("/workout/:id/delete", workout.remove)
+workoutRoutes.delete("/workout/:id", workout.remove)
 
 export default workoutRoutes;
