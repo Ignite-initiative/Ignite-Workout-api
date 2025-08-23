87cac4445291a630be404485eb7ca1a7a7867ebd
Pedro Henrique <76178088+pedrohenrique11@users.noreply.github.com>
2025-08-23 00:10:16 +0000

    refactor: add authenticate token middleware

--- src/routes/workout-routes.ts
+++ src/routes/workout-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import * as workout from "../controllers/workout-controller";
+import authenticateToken from "../middlewares/authenticateToken";
 
 const workoutRoutes = Router()
 
-workoutRoutes.post("/workout/register", workout.register)
-workoutRoutes.delete("/workout/:id", workout.remove)
+workoutRoutes.post("/workout/register", authenticateToken, workout.register)
+workoutRoutes.delete("/workout/:id", authenticateToken, workout.remove)
 
 export default workoutRoutes;
