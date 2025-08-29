7a029020a71e8b4423d6e83a6dddd03e4acb7601
Pedro <pedrohenriqueb.dev@gmail.com>
2025-08-29 06:56:20 +0000

    feat: add patch workout route

--- src/routes/workout-routes.ts
+++ src/routes/workout-routes.ts
@@ -5,6 +5,7 @@ import authenticateToken from "../middlewares/authenticateToken";
 const workoutRoutes = Router()
 
 workoutRoutes.post("/workout/register", authenticateToken, workout.register)
+workoutRoutes.patch("/workout/:id", authenticateToken, workout.update)
 workoutRoutes.delete("/workout/:id", authenticateToken, workout.remove)
 
 export default workoutRoutes;
