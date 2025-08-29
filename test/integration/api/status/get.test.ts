35878e565a4343d794041ead772ed2dd69f38e21
Leonardo <leonardolccabral@gmail.com>
2025-08-29 00:05:57 -0300

    feat: increase integration tests with `concurrently` and `async-retry`

--- test/integration/api/status/get.test.ts
+++ test/integration/api/status/get.test.ts
@@ -1,16 +1,21 @@
 import { resourceLimits } from "worker_threads"
 import prisma from "../../../../src/utils/prismaClient"
+import orchestrator from '../../orchestrator';
+
+beforeAll(async () => {
+    await orchestrator.waitForAllServices();
+})
 
 describe("GET on /status", () => {
-    
+
     async function getStatus() {
-        
+
         const status = await fetch('http://localhost:3000/api/v1/status', {
             method: 'GET'
         })
         return status
     }
-    
+
     test("should return a status 200 on success", async () => {
         const result = await getStatus();
         const convertResult = await result.json()
@@ -22,9 +27,9 @@ describe("GET on /status", () => {
         const convertResult = await result.json()
         const versionResult = convertResult.dependencies.database.version
 
-        const version = await prisma.$queryRawUnsafe<any> (
+        const version = await prisma.$queryRawUnsafe<any>(
             'SHOW server_version'
-        ) 
+        )
 
         expect(versionResult).toBe(version[0].server_version)
     })
