365b58d9da89a0627a0264f2e2e98226f9cd1b18
Pedro <pedrohenriqueb.dev@gmail.com>
2025-09-01 18:06:45 +0000

    fix: response status code

--- src/controllers/exercise-controller.ts
+++ src/controllers/exercise-controller.ts
@@ -3,13 +3,13 @@ import { ExerciseModel } from "../models/Exercise";
 
 const exerciseModel = new ExerciseModel
 
-export default async function create(req: Request, res: Response) {
+export async function create(req: Request, res: Response) {
     const data = req.body
 
     try {
         await exerciseModel.create(data)
 
-        res.status(200).send("Exercise Created")
+        res.status(201).send("Exercise Created")
     }
     catch(error) {
         res.status(401).send({ message: error })
