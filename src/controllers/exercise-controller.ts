8d9e79ce5661fa80119d905534ee8f3f531772b0
Pedro <pedrohenriqueb.dev@gmail.com>
2025-09-01 21:14:47 +0000

    feat: add update exercise controller

--- src/controllers/exercise-controller.ts
+++ src/controllers/exercise-controller.ts
@@ -15,3 +15,18 @@ export async function create(req: Request, res: Response) {
         res.status(401).send({ message: error })
     }
 }
+
+export async function update(req: Request, res: Response) {
+    const { id } = req.params
+    const data = req.body
+
+    try {
+        await exerciseModel.update(data, id)
+
+        res.status(201).send("Exercise Updated")
+    }
+    catch(error) {
+        res.status(401).send({ message: error }
+        )
+    }
+}
