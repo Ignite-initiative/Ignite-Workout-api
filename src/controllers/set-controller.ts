8716fb507e89944167bc7cb199b1cc8efe725b42
Pedro <pedrohenriqueb.dev@gmail.com>
2025-09-03 16:37:04 +0000

    feat: add update set controller

--- src/controllers/set-controller.ts
+++ src/controllers/set-controller.ts
@@ -3,12 +3,25 @@ import { SetsModel } from "../models/Sets";
 
 const setModel = new SetsModel
 
-export default async function create(req: Request, res: Response) {
-    const { id } = req.params
+export async function create(req: Request, res: Response) {
     const  { data } = req.body
 
     try {
         await setModel.create(data)
+
+        res.status(201).send("Set created")
+    }
+    catch(error) {
+        res.status(401).send({ message: error })
+    }
+}
+
+export async function update(req: Request, res: Response) {
+    const { id } = req.params
+    const  { data } = req.body
+
+    try {
+        await setModel.update(data, id)
 
         res.status(201).send("Set created")
     }
