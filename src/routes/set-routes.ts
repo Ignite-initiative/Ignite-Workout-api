473bae503f0f0d8fd12cab0e8b9439e7555179b7
Pedro <pedrohenriqueb.dev@gmail.com>
2025-09-03 16:42:39 +0000

    feat: add create set route

---
+++ src/routes/set-routes.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import authenticateToken from "../middlewares/authenticateToken";
+import * as set from "../controllers/set-controller"
+
+const setRouter = Router()
+
+setRouter.post("/set", authenticateToken, set.create)
