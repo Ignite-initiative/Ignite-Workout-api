902d53b1d807a53efdaa2c61c30a4b2cbef61700
Pedro <pedrohenriqueb.dev@gmail.com>
2025-09-04 21:46:11 +0000

    test: post in set route

--- test/integration/api/set/post.test.ts
+++ test/integration/api/set/post.test.ts
@@ -1,13 +1,12 @@
-import prisma from '../../../../src/utils/prismaClient';
-import jwt from 'jsonwebtoken'
-import orchestrator from '../../orchestrator';
+import prisma from "../../../../src/utils/prismaClient"
+import jwt from "jsonwebtoken"
 
-describe("DELETE on /exercise", () => {
+describe("POST on /set", () => {
     let exerciseId: string
     let token: string
 
     beforeAll(async () => {
-        await orchestrator.waitForAllServices();
+        await prisma.set.deleteMany()
         await prisma.exercise.deleteMany()
         await prisma.workout.deleteMany()
         await prisma.user.deleteMany()
@@ -15,7 +14,7 @@ describe("DELETE on /exercise", () => {
         const user = await prisma.user.create({
             data: {
                 name: "Pedro",
-                email: "teste@email.com",
+                email: "teste_exercicio@email.com",
                 password_hash: "hshsshhs",
                 telephone: "61 12345-1234",
                 height: 1.67,
@@ -26,7 +25,7 @@ describe("DELETE on /exercise", () => {
         const workout = await prisma.workout.create({
             data: {
                 name: "treino 1",
-                date: new Date("2025-11-11"),
+                date: new Date('2011-11-11'),
                 isCompleted: false,
                 userId: user.id
             }
@@ -34,7 +33,7 @@ describe("DELETE on /exercise", () => {
 
         const exercise = await prisma.exercise.create({
             data: {
-                name: "bench press",
+                name: "exercise 1",
                 category: "chest",
                 workoutId: workout.id
             }
@@ -47,45 +46,49 @@ describe("DELETE on /exercise", () => {
                 userId: user.id,
                 email: user.email,
             },
-            process.env.JWT_SECRET_KEY || "testkey",
+            process.env.JWT_SECRET_KEY || "secretkey",
             { expiresIn: '12h' }
         );
-
-
     })
-    afterAll(async () => {
+
+    afterEach(async () => {
+        await prisma.set.deleteMany()
         await prisma.exercise.deleteMany()
         await prisma.workout.deleteMany()
         await prisma.user.deleteMany()
         await prisma.$disconnect()
     })
 
+    type CreateSetPayload = {
+        category: string,
+        repsPlanned: number,
+        weightPlanned: number,
+        rir: number,
+        exerciseId: string
+    }
 
-    async function deleteExercise(id: string) {
-        const result = await fetch(`http://localhost:3000/api/v1/exercise/${id}`, {
-            method: 'DELETE',
+    async function createSet(set: CreateSetPayload) {
+        const result = await fetch('http://localhost:3000/api/v1/set', {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${token}`
             },
+            body: JSON.stringify(set)
         })
-
-        return result
+        return result;
     }
 
-    test("Should be return 200 if workout is deleted", async () => {
-        const result = await deleteExercise(exerciseId)
+    test("Should return a status 201 at success", async () => {
+        const set = {
+            category: "valid",
+            repsPlanned: 10,
+            weightPlanned: 100,
+            rir: 1,
+            exerciseId: exerciseId
+        }
+        const result = await createSet(set)
 
         expect(result.status).toBe(201)
-    })
-    test("should be return 401 if user does not have access", async () => {
-        const result = await fetch(`http://localhost:3000/api/v1/exercise/${exerciseId}`, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer token_invalido"
-            }
-        })
-        expect(result.status).toBe(401)
     })
 })
